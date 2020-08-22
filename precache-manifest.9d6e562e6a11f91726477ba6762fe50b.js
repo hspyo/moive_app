61a17d814796f6b046113899ae69662a889c6720
Pyo <olok95@naver.com>
2020-08-22 11:45:00 +0900

    Updates

---
+++ precache-manifest.9d6e562e6a11f91726477ba6762fe50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee4ecc9e1a51d78e6b12c0d6141c7ac",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fdc183cfc747c0395503",
+    "url": "/movie_app/static/css/main.e9715910.chunk.css"
+  },
+  {
+    "revision": "b3e2cf7259859ddd7c70",
+    "url": "/movie_app/static/js/2.02cd0f0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.02cd0f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc183cfc747c0395503",
+    "url": "/movie_app/static/js/main.3aa3a9cd.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
