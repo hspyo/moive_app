06870391808024c8dabc560e3a0fa391ae98583b
Pyo <olok95@naver.com>
2020-08-22 15:23:31 +0900

    ADD sharing props between routes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,28 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 console.log("movie");
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title} />{" "}
-      {/* title은 이미지에 hover시 제목 나타냄 */}
-      <div className="movie__data">
-        <h3 className="movie__data">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary.slice(0,180)}...</p>
-      </div>
-      <div>{console.log("movieRender ")}</div>
-    </div>
-  );
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres
+        }
+      }}
+      >
+        <div className="movie">
+          <img src={poster} alt={title} title={title} />
+          <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="movie__genres">
+              {genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
+          </div>
+        </div>
+        <div>{console.log("movieRender ")}</div>
+      </Link>
+   );
 }
 
 Movie.propsTypes = {
