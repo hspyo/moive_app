91cf8d9d89f7373cb8282399060f74bacde921d3
Pyo <olok95@naver.com>
2020-08-22 10:56:28 +0900

    ADD styles

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import "./Movie.css";
 console.log("movie");
 
 function Movie({ year, title, summary, poster, genres }) {
-  
-  return ( 
+  return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} /> {/* title은 이미지에 hover시 제목 나타냄 */}
+      <img src={poster} alt={title} title={title} />{" "}
+      {/* title은 이미지에 hover시 제목 나타냄 */}
       <div className="movie__data">
         <h3 className="movie__data">{title}</h3>
         <h5 className="movie__year">{year}</h5>
@@ -18,9 +18,9 @@ function Movie({ year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0,180)}...</p>
       </div>
-      <div>{console.log("movieRender")}</div>
+      <div>{console.log("movieRender ")}</div>
     </div>
   );
 }
@@ -32,7 +32,7 @@ Movie.propsTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
