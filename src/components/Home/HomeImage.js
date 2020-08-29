c9fc0833e7bd69f2100f72d94252a7d8cf91ca44
Pyo <olok95@naver.com>
2020-08-29 12:09:38 +0900

    UPDATE HomeImage Size

--- src/components/Home/HomeImage.js
+++ src/components/Home/HomeImage.js
@@ -10,7 +10,7 @@ function HomeImage(props) {
  41%,rgba(0,0,0,0.65)
  100%),
  url('${props.image}'), #1c1c1c`,
-        height: "650px",
+        height: "750px",
         backgroundSize: "100%, cover",
         backgroundPosition: "center, center",
         width: "100%",
