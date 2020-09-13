286c9c8719aff005ec47c1ef6015c04932b5931e
hspyo <olok95@naver.com>
2020-09-13 16:14:13 +0900

    Updates

---
+++ precache-manifest.d16f82ffded0df65033120adc8bc1799.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88cdf7532f1744f22ef0302bcb3ba78",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ea2dae736bffaca181a0",
+    "url": "/movie_app/static/css/main.a9a4c023.chunk.css"
+  },
+  {
+    "revision": "2358e47aee3b93b7a55a",
+    "url": "/movie_app/static/js/2.d390fd48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.d390fd48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2dae736bffaca181a0",
+    "url": "/movie_app/static/js/main.b084e4d0.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  },
+  {
+    "revision": "aaae8ca3783eab1102efe4b37b93ee5d",
+    "url": "/movie_app/static/media/nophoto.aaae8ca3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.5c69fbd9c13804ab4b1fe3242416168a.js"
+  "/movie_app/precache-manifest.d16f82ffded0df65033120adc8bc1799.js"
 );
 
 self.addEventListener('message', (event) => {
